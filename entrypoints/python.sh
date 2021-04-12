0a6956185190a5b2ade63919510a7bd750c1ffa7
Chris <github@mrz.angrydome.com>
2021-04-12 12:40:09 +0100

    staging

--- entrypoints/python.sh
+++ entrypoints/python.sh
@@ -68,7 +68,7 @@ export -f PipfilePrep
 poetryPrep(){
     set +o noglob
     setDebug
-    pipenvInstall
+    poetryInstall
 
     FILENAME=$(basename "$1")
     DIRECTORY=$(dirname "$1")
@@ -79,25 +79,67 @@ poetryPrep(){
     then
         customPrep
     else
-        if [ -f "Pipfile.lock" ]
+        if ! [ -f "poetry.lock" ]
         then
-            pipenv sync
-        else
-            pipenv update
+            poetry lock --no-update
         fi
     fi
-    snyk_monitor "$FILENAME" "pip" "$PROJECT_PREFIX/$FILENAME"
+    snyk_monitor "$FILENAME" "poetry" "$PROJECT_PREFIX/$FILENAME"
     cd "$BASE"
 }
+export -f poetryPrep
 
-# setupPrep(){
+setupPrep(){
+    set +o noglob
+    setDebug
 
-# }
+    FILENAME=$(basename "$1")
+    DIRECTORY=$(dirname "$1")
+    PROJECT_PREFIX=${DIRECTORY#"$TARGET"}
 
-# reqPrep(){
+    cd "$DIRECTORY"
+    if [ -f ".snyk.d/prep.sh" ]
+    then
+        customPrep
+    elif ! [[ -f "requirements.txt" ]]; then
+        if ! [[ -d 'snyktmp' ]]; then
+            virtualenv snyktmp
+        fi
+        source snyktmp/bin/activate
+        pip install -U -e ./ && pip freeze > requirements.txt
+        snyk_monitor "requirements.txt" "pip" "$PROJECT_PREFIX/$FILENAME"
+        deactivate
+    fi
+    
+    cd "$BASE"
+}
+export -f setupPrep
 
-# }
+reqPrep(){
+    set +o noglob
+    setDebug
 
+    FILENAME=$(basename "$1")
+    DIRECTORY=$(dirname "$1")
+    PROJECT_PREFIX=${DIRECTORY#"$TARGET"}
+
+    cd "$DIRECTORY"
+    if [ -f ".snyk.d/prep.sh" ]
+    then
+        customPrep
+    else
+        if ! [[ -d 'snyktmp' ]]; then
+            virtualenv snyktmp
+        fi
+        source snyktmp/bin/activate
+        pip install -r requirements.txt
+        snyk_monitor "$FILENAME" "pip" "$PROJECT_PREFIX/$FILENAME"
+        deactivate
+    fi
+    
+    cd "$BASE"
+}
+export -f reqPrep
 
 findPipfile(){
     # we don't want bash expansion of * to happen in our find string, we reset noglob at the start of our functions
@@ -109,14 +151,31 @@ findPipfile(){
 findPoetry(){
     # we don't want bash expansion of * to happen in our find string, we reset noglob at the start of our functions
     set -o noglob
-    find "${TARGET}" -type f -name "pyproject.toml" $IGNORES -exec bash -c 'echoFile "$0"' {} \;
+    find "${TARGET}" -type f -name "pyproject.toml" $IGNORES -exec bash -c 'poetryPrep "$0"' {} \;
     set +o noglob
 }
+
+findReq(){
+    # we don't want bash expansion of * to happen in our find string, we reset noglob at the start of our functions
+    set -o noglob
+    find "${TARGET}" -type f -name "requirements.txt" $IGNORES -exec bash -c 'reqPrep "$0"' {} \;
+    set +o noglob
+}
+
+findSetup(){
+    # we don't want bash expansion of * to happen in our find string, we reset noglob at the start of our functions
+    set -o noglob
+    find "${TARGET}" -type f -name "setup.py" $IGNORES -exec bash -c 'setupPrep "$0"' {} \;
+    set +o noglob
+}
+
 
 # we check for .snyk.d/exclude folder in the root of the target
 # this is the ignore for all find requests and precludes the need for --exclude in snyk itself
 IGNORES=""
 snyk_excludes "${TARGET}" IGNORES
 
-findPipfile
-findPoetry
+#findPipfile
+#findPoetry
+#findReq
+findSetup
