e507e981276a37950f24819f431e60ff251acce2
Chris <github@mrz.angrydome.com>
2021-04-16 15:53:04 +0100

    Moves incomplete languages and experiements to experimental branch

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
-#/bin/bash 
+#!/bin/bash 
 set -x
 LABEL="python-development-$(git rev-parse HEAD)"
 docker image build --file Dockerfile-python -t $LABEL ./
-docker run -it -e SNYK_TOKEN -e BULK_DEBUG='true' --volume $(pwd):/home/dev --entrypoint /bin/bash $LABEL
+docker run -it -e SNYK_TOKEN -e SNYK_BULK_DEBUG='true' -v $(pwd):/home/dev --entrypoint=/bin/bash mrzarquon/snyk-bulk:python
