60a62251e147f624d058e8ce18cbc8a3887c6c25
roberthicks <roberthicksiii@gmail.com>
2021-08-27 23:20:32 -0400

    add .NET code for support.  Still need to fix find command

---
+++ testrepo/dotNet-goof/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotNet_goof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
